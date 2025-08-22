7bb0aab79c9e514d7063f5c041f0dcad5c9b2325
Matheus Gurther <matheusgurther@outlook.com>
2025-08-22 00:06:26 -0300

    Configurando TEMPLATES

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from galeria.views import index
 
-url_patterns = [
+urlpatterns = [
     path('', index)
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1>Galeria</h1><p>Bem-vindo à galeria!</p>')
+    return render(request, 'index.html')
