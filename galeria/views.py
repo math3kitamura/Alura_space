2c5e5583aed61482d852b80e3e8ef6d8691b5f8c
Matheus Gurther <matheusgurther@outlook.com>
2025-08-25 15:13:50 -0300

    Criando o banco de dados com models

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+dados ={
+    1: {"nome": "Nebulosa", "legenda": "webbtelecope.org / NASA / James Webb"},
+    2: {"nome": "Galáxia NGC 1019", "legenda": "nasa.org / NASA / Hubble"}
+}
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
