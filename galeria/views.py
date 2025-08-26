d8d09b662d23a831609fdd1e8e302f77642e2841
Matheus Gurther <matheusgurther@outlook.com>
2025-08-26 14:37:12 -0300

    Passando items do banco de dados para o HTML

--- galeria/views.py
+++ galeria/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 
-dados ={
-    1: {"nome": "Nebulosa", "legenda": "webbtelecope.org / NASA / James Webb"},
-    2: {"nome": "Galáxia NGC 1019", "legenda": "nasa.org / NASA / Hubble"}
-}
+from .models import Fotografia
 
 def index(request):
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
