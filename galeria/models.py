eee92c65a5d9633bca44a24969d7dc3554afc1d1
Matheus Gurther <matheusgurther@outlook.com>
2025-08-29 01:23:19 -0300

    Criando funçao para adicionar imagens no Admin

--- galeria/models.py
+++ galeria/models.py
@@ -14,7 +14,7 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=255, null = False, blank = False)
     categoria = models.CharField(max_length=100, choices= OPCOES_CATEGORIA, default="")
     descricao = models.TextField(null = True, blank = True)
-    foto = models.CharField(max_length=100, null = False, blank = False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
