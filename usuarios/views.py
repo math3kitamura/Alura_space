4b10e8d6687b57a2c9e73db507ea5586b3e034d8
Matheus Gurther <matheusgurther@outlook.com>
2025-09-05 02:02:17 -0300

    Corrigindo falta de , em views

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,7 @@ from usuarios.forms import LoginForms
 
 def login(request):
     form = LoginForms()
-    return render(request, 'usuarios/login.html' {'form': form})
+    return render(request, 'usuarios/login.html', {'form': form})
 
 def cadastro(request):
     return render(request, 'usuarios/cadastro.html')
