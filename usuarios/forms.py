2db046928e1187af7ee77ea5107c7f14cec904a9
Matheus Gurther <matheusgurther@outlook.com>
2025-09-05 02:20:45 -0300

    Melhorando identacao e usando dicionario do Django para criar atributos HTML

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,5 +1,25 @@
 from django import forms
 
 class LoginForms(forms.Form):
-    nome_login = forms.CharField(label='Nome de login', required=True, max_length=100)
-    senha = forms.CharField(label='Senha', required=True, max_length=70, widget=forms.PasswordInput())
+    nome_login = forms.CharField(
+        label='Nome de login: ', 
+        required=True, 
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite seu nome de login',
+            }
+        )
+    )
+    senha = forms.CharField(
+        label='Senha: ', 
+        required=True, 
+        max_length=70, 
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha',
+            }
+        )
+    )
