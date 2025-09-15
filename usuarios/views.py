57f5b8f727222b9ae45d14443c9988cfdb0ba2e1
Matheus Gurther <matheusgurther@outlook.com>
2025-09-15 20:31:39 -0300

    Criando a logica de cadastro de usuarios, conectando com banco de dados do Django

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
 
 def login(request):
     form = LoginForms()
@@ -10,8 +11,25 @@ def cadastro(request):
     
     if request.method == 'POST':
         form = CadastroForms(request.POST)
+         
+        if form.is_valid():
+            if form["senha_1"].value() != form["senha_2"].value():
+                return redirect('cadastro')
         
-        if form["senha_1"].value() != form["senha_2"].value():
-            return redirect('cadastro')
-    
+            nome= form["nome_cadastro"].value()
+            email= form["email"].value()
+            senha= form["senha_1"].value()
+            
+            if User.objects.filter(username=nome).exists():
+                return redirect('cadastro')
+            
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+            
+            usuario.save()
+            return redirect('login')
+                        
     return render(request, 'usuarios/cadastro.html', {'form': form})
