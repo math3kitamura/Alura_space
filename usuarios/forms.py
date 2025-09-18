66797ed4685e8f5aece2aee793bc62da417dca5a
Matheus Gurther <matheusgurther@outlook.com>
2025-09-18 01:42:38 -0300

    Adicionando validação simples

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -72,3 +72,13 @@ class CadastroForms(forms.Form):
             }
         )
     )
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get('nome_cadastro')
+        
+        if nome:
+            nome = nome.strip()
+            if ' ' in nome:
+                raise forms.ValidationError('Nome de cadastro não pode conter espaços.')
+            else:
+                return nome
